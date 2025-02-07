c6311f73bf3f7417931d0fbe159d231d725c71c1
gabriele <gabriele.fantini@blindata.io>
2025-02-07 15:53:04 +0100

    Changed OdmCliBaseConfiguration structure

--- src/main/java/org/opendatamesh/cli/extensions/OdmCliBaseConfiguration.java
+++ src/main/java/org/opendatamesh/cli/extensions/OdmCliBaseConfiguration.java
@@ -1,37 +1,27 @@
 package org.opendatamesh.cli.extensions;
 
+import java.util.List;
 import java.util.Map;
 
 public class OdmCliBaseConfiguration {
-
-    private String templateFolder;
-
-    private Map<String, Map<String, String>> services;
-    private Map<String, Map<String, String>> systems;
+    private Config cliConfiguration;
+    private List<System> remoteSystemsConfigurations;
     private Map<String, String> env;
 
-    public String getTemplateFolder() {
-        return templateFolder;
+    public Config getCliConfiguration() {
+        return cliConfiguration;
     }
 
-    public void setTemplateFolder(String templateFolder) {
-        this.templateFolder = templateFolder;
+    public void setCliConfiguration(Config cliConfiguration) {
+        this.cliConfiguration = cliConfiguration;
     }
 
-    public Map<String, Map<String, String>> getServices() {
-        return services;
+    public List<System> getRemoteSystemsConfigurations() {
+        return remoteSystemsConfigurations;
     }
 
-    public void setServices(Map<String, Map<String, String>> services) {
-        this.services = services;
-    }
-
-    public Map<String, Map<String, String>> getSystems() {
-        return systems;
-    }
-
-    public void setSystems(Map<String, Map<String, String>> systems) {
-        this.systems = systems;
+    public void setRemoteSystemsConfigurations(List<System> remoteSystemsConfigurations) {
+        this.remoteSystemsConfigurations = remoteSystemsConfigurations;
     }
 
     public Map<String, String> getEnv() {
@@ -40,5 +30,56 @@ public class OdmCliBaseConfiguration {
 
     public void setEnv(Map<String, String> env) {
         this.env = env;
+    }
+
+    public static class System {
+        private String name;
+        private String endpoint;
+        private String user;
+        private String password;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEndpoint() {
+            return endpoint;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setEndpoint(String endpoint) {
+            this.endpoint = endpoint;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    public static class Config {
+        private String templatesFolder;
+
+        public String getTemplatesFolder() {
+            return templatesFolder;
+        }
+
+        public void setTemplatesFolder(String templatesFolder) {
+            this.templatesFolder = templatesFolder;
+        }
     }
 }
